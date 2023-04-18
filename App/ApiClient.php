b8a7c77e3432abdb577d859408c7b786a21b914c
walle <valters.jargans@gmail.com>
2023-04-18 19:17:50 +0300

    Added UR - App

--- App/ApiClient.php
+++ App/ApiClient.php
@@ -1,42 +1,54 @@
 <?php
 
-
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 
 class ApiClient
 {
     private Client $client;
+
     public function __construct()
     {
-        $this->client = new Client(['verify' => false ]);
+        $this->client = new Client();
     }
 
-    public function getRecords(string $parameter): array
+    /**
+     * Returns an array of Data objects matching the provided registration code.
+     *
+     * @param string $registrationCode The registration code to search for.
+     * @return array An array of Data objects.
+     * @throws GuzzleException|JsonException If the API request fails or returns unexpected data.
+     */
+    public function getRecords(string $registrationCode): array
     {
         $key = "&resource_id=25e80bf3-f107-4ab4-89ef-251b5b9374e9";
-        $query = "q=$parameter";
+        $query = "q=$registrationCode";
         $url = "https://data.gov.lv/dati/lv/api/3/action/datastore_search?q=" . $query . $key;
 
         try {
             $response = $this->client->request('GET', $url);
         } catch (GuzzleException $e) {
-            echo $e . PHP_EOL;
+            throw GuzzleException("Error retrieving data from the API: " . $e->getMessage(), $e->getCode(), $e);
         }
 
-        $companyData = json_decode($response->getBody()->getContents());
+        $responseData = json_decode($response->getBody()->getContents());
 
-        $foundedRecords = [];
-        foreach ($companyData->result->records as $record) {
-            if ($record->regcode === $parameter)
-                $foundedRecords[] = new Data(
+        if (!isset($responseData->result->records)) {
+            throw new JsonException("Unexpected API response data: " . json_encode($responseData));
+        }
+
+        $foundRecords = [];
+        foreach ($responseData->result->records as $record) {
+            if ($record->regcode === $registrationCode) {
+                $foundRecords[] = new Data(
                     $record->name,
                     $record->regcode,
                     $record->address,
                     $record->registered
                 );
-
+            }
         }
-        return $foundedRecords;
+
+        return $foundRecords;
     }
 }
--- App/Application.php
+++ App/Application.php
@@ -4,22 +4,51 @@ require_once '../vendor/autoload.php';
 require "ApiClient.php";
 require "Data.php";
 
-$query = readline('Enter registration number: ');
+class Application {
+    private $apiClient;
+
+    public function __construct(ApiClient $apiClient) {
+        $this->apiClient = $apiClient;
+    }
+
+    public function run() {
+        $query = readline('Enter registration number: ');
+
+        // Validate user input
+        if (!$this->validateInput($query)) {
+            echo 'Invalid input. Please enter a valid registration number.' . PHP_EOL;
+            return;
+        }
+
+        $records = $this->apiClient->getRecords($query);
+
+        // Handle no results
+        if (!$records) {
+            echo 'Results not found.' . PHP_EOL;
+            return;
+        }
+
+        $this->displayRecords($records);
+    }
+
+    private function validateInput(string $input): bool {
+        // TODO: Implement validation logic
+        return true;
+    }
+
+    private function displayRecords(array $records) {
+        // TODO: Implement formatting logic
+        foreach ($records as $record) {
+            echo "Company name: {$record->getName()}" . PHP_EOL;
+            echo "Registration number: {$record->getRegNumber()}" . PHP_EOL;
+            echo "Address: {$record->getAddress()}" . PHP_EOL;
+            $date = substr($record->getRegDate(), 0, 10);
+            echo "Registration date: $date" . PHP_EOL;
+            echo PHP_EOL;
+        }
+    }
+}
 
 $apiClient = new ApiClient();
-$records = $apiClient->getRecords($query);
-
-echo PHP_EOL;
-
-if (!$records) {
-    echo 'Results not found.';
-}
-
-foreach ($records as $record) {
-    echo "Company name: {$record->getName()}" . PHP_EOL;
-    echo "Registration number: {$record->getRegNumber()}" . PHP_EOL;
-    echo "Address: {$record->getAddress()}" . PHP_EOL;
-    $date = substr($record->getRegDate(), 0, 10);
-    echo "Registration date: $date" . PHP_EOL;
-    echo PHP_EOL;
-}
+$app = new Application($apiClient);
+$app->run();
